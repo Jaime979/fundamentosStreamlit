48a1aae46914fd11295a60412999972c99537d6b
Jaime979 <sesenpuma@gmail.com>
2026-07-07 21:27:00 -0500

    Add loan calculation inputs and result display

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ st.title("Aplicación de Fundamento Streamlist")
 st.sidebar.image("logo.png")
 
 st.sidebar.title("parametros")
+
+
+monto = st.number_input("ingrese el monto de su prestamo")
+
+interes = st.number_input("ingrese el interes")
+
+tiempo_meses st.number_input("ingrese el tiempo en meses")
+
+resultado = monto*interes*(tiempo_meses/12)
+
+st.write("El resultado es:",resultado)
