bf25fc032ba140183811b9f9f86d5ffb8eab7d68
Jaime979 <sesenpuma@gmail.com>
2026-07-07 21:36:04 -0500

    Fix syntax for number_input parameters in app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ st.sidebar.image("logo.png")
 st.sidebar.title("parametros")
 
 
-monto = st.number_input("ingrese el monto de su prestamo" value = 5000)
+monto = st.number_input("ingrese el monto de su prestamo", value = 5000)
 
-interes = st.number_input("ingrese el interes" value = 0.05)
+interes = st.number_input("ingrese el interes", value = 0.05)
 
-tiempo_meses = st.number_input("ingrese el tiempo en meses" value = 12)
+tiempo_meses = st.number_input("ingrese el tiempo en meses",value = 12)
 
 resultado = monto*interes*(tiempo_meses/12)
 
