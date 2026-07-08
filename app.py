c21f4b03add06d659510be029de7ebd595be0d68
Jaime979 <sesenpuma@gmail.com>
2026-07-07 21:34:12 -0500

    Change default loan amount from 50000 to 5000

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.sidebar.image("logo.png")
 st.sidebar.title("parametros")
 
 
-monto = st.number_input("ingrese el monto de su prestamo" value = 50000)
+monto = st.number_input("ingrese el monto de su prestamo" value = 5000)
 
 interes = st.number_input("ingrese el interes" value = 0.05)
 
